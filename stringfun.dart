5395587ae88afdbfb89e6e3f7025351de1c4925f
Akash Ramesh <akashnairr2003@gmail.com>
2024-06-24 08:59:09 +0530

    commited

---
+++ stringfun.dart
@@ -0,0 +1,15 @@
+void main(){
+    String x="abcdefg";
+    int y=x.length;
+    print(y);
+    bool z=x.isEmpty;
+    print(z);
+    bool a=x.isNotEmpty;
+    print(a);
+    List<String>b=x.split("d");
+    print(b);
+    String c=x.substring(2,5);
+    print(c);
+
+
+}
