208249403bea7703a9ae9add3371b182f27c6977
Akash Ramesh <akashnairr2003@gmail.com>
2024-06-26 08:40:48 +0530

    commited

--- work.dart
+++ work.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
 void main(){
-    List<String> v = ["a","e","i","o","u"];
-
-    print("Enter text");
-    String str =stdin.readLineSync()!;
-    int count = str.split(" ").fold(0, (x,y) => v.contains(y[0])? x+1 : x);
-    print("There are $count vowel");
+    String x= " Akash R Nair ";
+    print(">$x<");
+    print(x.toUpperCase());
+    print(x.toLowerCase());
+    print(x.trim());
+    print(x.trimLeft());
+    print(x.trimRight());
+    print(x.lastIndexOf("a"));
 }
